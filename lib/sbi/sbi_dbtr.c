8e50b75ea91fb7cd7956b31288a59a73ccf0e187
Ka Leung Ho <ka.ho@codasip.com>
2024-11-27 10:01:07 +0000

    lib: sbi: dbtr: hart_shmem_base() supports Zcheripurecap ABI
    hart_shmem_base() return a pointer with maximun debug trigger entry and
    the read/write permission for Zcheripurecap ABI.

--- lib/sbi/sbi_dbtr.c
+++ lib/sbi/sbi_dbtr.c
@@ -19,7 +19,7 @@
 #include <sbi/sbi_hart_protection.h>
 #include <sbi/riscv_encoding.h>
 #include <sbi/riscv_asm.h>
-
+#include <sbi/riscv_cheri.h>
 
 /** Offset of pointer to HART's debug triggers info in scratch space */
 static unsigned long hart_state_ptr_offset;
@@ -71,8 +71,14 @@ static inline void sbi_dbtr_disable_shmem(
 static inline void *hart_shmem_base(
 	struct sbi_dbtr_hart_triggers_state *hs)
 {
+#if defined(__CHERI_PURE_CAPABILITY__)
+	return ((void *)cheri_build_cap_rw(
+		(unsigned long)DBTR_SHMEM_MAKE_PHYS(hs->shmem.phys_hi, hs->shmem.phys_lo),
+		hs->total_trigs * sizeof(union sbi_dbtr_shmem_entry)));
+#else
 	return ((void *)(unsigned long)DBTR_SHMEM_MAKE_PHYS(
 			hs->shmem.phys_hi, hs->shmem.phys_lo));
+#endif
 }
 
 static void sbi_trigger_init(struct sbi_dbtr_trigger *trig,
