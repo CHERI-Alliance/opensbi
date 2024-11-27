8cd14d25d5b4cdf9b6f830b3822d141c508a71b6
Ka Leung Ho <ka.ho@codasip.com>
2024-11-27 10:01:07 +0000

    lib: utils: Extend fdt_parse_compat_addr() to fdt_parse_compat_addr_size()
    The fdt_parse_compat_addr() is extended to fdt_parse_compat_addr_size()
    which allows getting size of relative field from FDT for ioremap().

--- lib/utils/fdt/fdt_helper.c
+++ lib/utils/fdt/fdt_helper.c
@@ -1147,7 +1147,7 @@ int fdt_parse_plicsw_node(const void *fdt, int nodeoffset, unsigned long *plicsw
 	return 0;
 }
 
-int fdt_parse_compat_addr(const void *fdt, uint64_t *addr,
+int fdt_parse_compat_addr_size(const void *fdt, uint64_t *addr, uint64_t *size,
 			  const char *compatible)
 {
 	int nodeoffset, rc;
@@ -1156,7 +1156,7 @@ int fdt_parse_compat_addr(const void *fdt, uint64_t *addr,
 	if (nodeoffset < 0)
 		return nodeoffset;
 
-	rc = fdt_get_node_addr_size(fdt, nodeoffset, 0, addr, NULL);
+	rc = fdt_get_node_addr_size(fdt, nodeoffset, 0, addr, size);
 	if (rc < 0 || !addr)
 		return SBI_ENODEV;
 
--- platform/generic/andes/ae350.c
+++ platform/generic/andes/ae350.c
@@ -94,7 +94,7 @@ static void ae350_hsm_device_init(const void *fdt)
 {
 	int rc;
 
-	rc = fdt_parse_compat_addr(fdt, (uint64_t *)&smu.addr,
+	rc = fdt_parse_compat_addr_size(fdt, (uint64_t *)&smu.addr, NULL,
 				   "andestech,atcsmu");
 
 	if (!rc) {
