CHERI-Alliance/opensbi

commit 0a94098384f363a4dfbccc476f9801ff7bcffa25

lib: utils/ipi: Use IO pointer return from ioreamp()
addr in struct aclint_mswi_data and struct plicsw_data are now a IO pointer
return from ioremap() to make ipi driver compatible with Zcheripurecap ABI.

lib/utils/ipi/fdt_ipi_mswi.c
@@ -12,6 +12,7 @@
 #include <sbi_utils/fdt/fdt_helper.h>
 #include <sbi_utils/ipi/fdt_ipi.h>
 #include <sbi_utils/ipi/aclint_mswi.h>
+#include <sbi/riscv_io.h>
 
 static int ipi_mswi_cold_init(void *fdt, int nodeoff,
 			      const struct fdt_match *match)
@@ -19,13 +20,14 @@ static int ipi_mswi_cold_init(void *fdt, int nodeoff,
 	int rc;
 	unsigned long offset;
 	struct aclint_mswi_data *ms;
+	uint64_t addr;
 
 	ms = sbi_zalloc(sizeof(*ms));
 	if (!ms)
 		return SBI_ENOMEM;
 
 	rc = fdt_parse_aclint_node(fdt, nodeoff, false, false,
-				   &ms->addr, &ms->size, NULL, NULL,
+				   &addr, &ms->size, NULL, NULL,
 				   &ms->first_hartid, &ms->hart_count);
 	if (rc) {
 		sbi_free(ms);
@@ -35,11 +37,13 @@ static int ipi_mswi_cold_init(void *fdt, int nodeoff,
 	if (match->data) {
 		/* Adjust MSWI address and size for CLINT device */
 		offset = *((unsigned long *)match->data);
-		ms->addr += offset;
+		addr += offset;
 		if ((ms->size - offset) < ACLINT_MSWI_SIZE)
 			return SBI_EINVAL;
 		ms->size = ACLINT_MSWI_SIZE;
 	}
+
+	ms->addr = ioremap(addr, ms->size);
 
 	rc = aclint_mswi_cold_init(ms);
 	if (rc) {

lib/utils/ipi/fdt_ipi_plicsw.c
@@ -21,11 +21,14 @@ int fdt_plicsw_cold_ipi_init(void *fdt, int nodeoff,
 				const struct fdt_match *match)
 {
 	int rc;
+	uint64_t addr;
 
-	rc = fdt_parse_plicsw_node(fdt, nodeoff, &plicsw.addr, &plicsw.size,
+	rc = fdt_parse_plicsw_node(fdt, nodeoff, &addr, &plicsw.size,
 				   &plicsw.hart_count);
 	if (rc)
 		return rc;
+
+	plicsw.addr = ioremap(addr, plicsw.size);
 
 	rc = plicsw_cold_ipi_init(&plicsw);
 	if (rc)

include/sbi_utils/ipi/aclint_mswi.h
@@ -20,7 +20,7 @@
 
 struct aclint_mswi_data {
 	/* Public details */
-	unsigned long addr;
+	void *addr;
 	unsigned long size;
 	u32 first_hartid;
 	u32 hart_count;

include/sbi_utils/ipi/andes_plicsw.h
@@ -27,7 +27,7 @@
 #define PLICSW_REGION_ALIGN	0x1000
 
 struct plicsw_data {
-	unsigned long addr;
+	void *addr;
 	unsigned long size;
 	uint32_t hart_count;
 };

lib/utils/ipi/aclint_mswi.c
@@ -88,7 +88,7 @@ int aclint_mswi_cold_init(struct aclint_mswi_data *mswi)
 	struct sbi_domain_memregion reg;
 
 	/* Sanity checks */
-	if (!mswi || (mswi->addr & (ACLINT_MSWI_ALIGN - 1)) ||
+	if (!mswi || ((unsigned long)mswi->addr & (ACLINT_MSWI_ALIGN - 1)) ||
 	    (mswi->size < (mswi->hart_count * sizeof(u32))) ||
 	    (!mswi->hart_count || mswi->hart_count > ACLINT_MSWI_MAX_HARTS))
 		return SBI_EINVAL;
@@ -117,7 +117,7 @@ int aclint_mswi_cold_init(struct aclint_mswi_data *mswi)
 	for (pos = 0; pos < mswi->size; pos += ACLINT_MSWI_ALIGN) {
 		region_size = ((mswi->size - pos) < ACLINT_MSWI_ALIGN) ?
 			      (mswi->size - pos) : ACLINT_MSWI_ALIGN;
-		sbi_domain_memregion_init(mswi->addr + pos, region_size,
+		sbi_domain_memregion_init((unsigned long)mswi->addr + pos, region_size,
 					  (SBI_DOMAIN_MEMREGION_MMIO |
 					   SBI_DOMAIN_MEMREGION_M_READABLE |
 					   SBI_DOMAIN_MEMREGION_M_WRITABLE),

lib/utils/ipi/andes_plicsw.c
@@ -20,7 +20,7 @@ struct plicsw_data plicsw;
 
 static void plicsw_ipi_send(u32 hart_index)
 {
-	ulong pending_reg;
+	void *pending_reg;
 	u32 interrupt_id, word_index, pending_bit;
 	u32 target_hart = sbi_hartindex_to_hartid(hart_index);
 
@@ -44,7 +44,7 @@ static void plicsw_ipi_send(u32 hart_index)
 static void plicsw_ipi_clear(u32 hart_index)
 {
 	u32 target_hart = sbi_hartindex_to_hartid(hart_index);
-	ulong reg = plicsw.addr + PLICSW_CONTEXT_BASE + PLICSW_CONTEXT_CLAIM +
+	void *reg = plicsw.addr + PLICSW_CONTEXT_BASE + PLICSW_CONTEXT_CLAIM +
 		    PLICSW_CONTEXT_STRIDE * target_hart;
 
 	if (plicsw.hart_count <= target_hart)
@@ -79,7 +79,7 @@ int plicsw_cold_ipi_init(struct plicsw_data *plicsw)
 {
 	int rc;
 	u32 interrupt_id, word_index, enable_bit;
-	ulong enable_reg, priority_reg;
+	void *enable_reg, *priority_reg;
 
 	/* Setup source priority */
 	for (int i = 0; i < plicsw->hart_count; i++) {
@@ -101,7 +101,8 @@ int plicsw_cold_ipi_init(struct plicsw_data *plicsw)
 	}
 
 	/* Add PLICSW region to the root domain */
-	rc = sbi_domain_root_add_memrange(plicsw->addr, plicsw->size,
+	rc = sbi_domain_root_add_memrange((unsigned long)plicsw->addr,
+					  plicsw->size,
 					  PLICSW_REGION_ALIGN,
 					  SBI_DOMAIN_MEMREGION_MMIO |
 					  SBI_DOMAIN_MEMREGION_M_READABLE |

commit 1afdd82573300a472cfa97f12faab2c049b7607f

Fix 32-bit build after 0a940983
Fixes: 0a940983 ("lib: utils/ipi: Use IO pointer return from ioreamp()")

lib/utils/ipi/fdt_ipi_mswi.c
@@ -20,7 +20,7 @@ static int ipi_mswi_cold_init(void *fdt, int nodeoff,
 	int rc;
 	unsigned long offset;
 	struct aclint_mswi_data *ms;
-	uint64_t addr;
+	unsigned long addr;
 
 	ms = sbi_zalloc(sizeof(*ms));
 	if (!ms)

lib/utils/ipi/fdt_ipi_plicsw.c
@@ -21,7 +21,7 @@ int fdt_plicsw_cold_ipi_init(void *fdt, int nodeoff,
 				const struct fdt_match *match)
 {
 	int rc;
-	uint64_t addr;
+	unsigned long addr;
 
 	rc = fdt_parse_plicsw_node(fdt, nodeoff, &addr, &plicsw.size,
 				   &plicsw.hart_count);